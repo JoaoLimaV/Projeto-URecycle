4f0d88f79593cc99ef6b0689512a3324f36c25d0
LeonardoLisboa <leonardo.lisboa2700@hotmail.com>
2022-06-11 23:37:48 -0300

    Perguntas Frequentes V Final

--- src/js/mudartexto.js
+++ src/js/mudartexto.js
@@ -1,39 +1,137 @@
 window.onload = () => {
-    document.getElementById("ajuda1").style.display = 'block';
-    document.getElementById("ajuda2").style.display = 'none';
+    if (window.matchMedia("(min-width:805px)").matches) {
+        document.getElementById("ajuda1").style.display = 'block';
+      } else {
+        document.getElementById("ajuda1mobile").style.display = 'block';
+      }
 }
+window.onresize = function(){
+  location.reload();
+};
 function mudatexto1() {
-    document.getElementById("ajuda1").style.display = 'block';
-    document.getElementById("ajuda2").style.display = 'none';
-    document.getElementById("ajuda3").style.display = 'none';
-    document.getElementById("ajuda4").style.display = 'none';
-    document.getElementById("ajuda5").style.display = 'none';
+    if (window.matchMedia("(min-width:805px)").matches) {
+        document.getElementById("ajuda1").style.display = 'block';
+        document.getElementById("ajuda2").style.display = 'none';
+        document.getElementById("ajuda3").style.display = 'none';
+        document.getElementById("ajuda4").style.display = 'none';
+        document.getElementById("ajuda5").style.display = 'none';
+        document.getElementById("ajuda1mobile").style.display = 'none';
+        document.getElementById("ajuda2mobile").style.display = 'none';
+        document.getElementById("ajuda3mobile").style.display = 'none';
+        document.getElementById("ajuda4mobile").style.display = 'none';
+        document.getElementById("ajuda5mobile").style.display = 'none';
+      } else {
+        document.getElementById("ajuda1").style.display = 'none';
+        document.getElementById("ajuda2").style.display = 'none';
+        document.getElementById("ajuda3").style.display = 'none';
+        document.getElementById("ajuda4").style.display = 'none';
+        document.getElementById("ajuda5").style.display = 'none';
+        document.getElementById("ajuda1mobile").style.display = 'block';
+        document.getElementById("ajuda2mobile").style.display = 'none';
+        document.getElementById("ajuda3mobile").style.display = 'none';
+        document.getElementById("ajuda4mobile").style.display = 'none';
+        document.getElementById("ajuda5mobile").style.display = 'none';
+      }
+    
 }
 function mudatexto2() {
-    document.getElementById("ajuda2").style.display = 'block';
-    document.getElementById("ajuda1").style.display = 'none';
-    document.getElementById("ajuda3").style.display = 'none';
-    document.getElementById("ajuda4").style.display = 'none';
-    document.getElementById("ajuda5").style.display = 'none';
+    if (window.matchMedia("(min-width:805px)").matches) {
+        document.getElementById("ajuda1").style.display = 'none';
+        document.getElementById("ajuda2").style.display = 'block';
+        document.getElementById("ajuda3").style.display = 'none';
+        document.getElementById("ajuda4").style.display = 'none';
+        document.getElementById("ajuda5").style.display = 'none';
+        document.getElementById("ajuda1mobile").style.display = 'none';
+        document.getElementById("ajuda2mobile").style.display = 'none';
+        document.getElementById("ajuda3mobile").style.display = 'none';
+        document.getElementById("ajuda4mobile").style.display = 'none';
+        document.getElementById("ajuda5mobile").style.display = 'none';
+      } else {
+        document.getElementById("ajuda1").style.display = 'none';
+        document.getElementById("ajuda2").style.display = 'none';
+        document.getElementById("ajuda3").style.display = 'none';
+        document.getElementById("ajuda4").style.display = 'none';
+        document.getElementById("ajuda5").style.display = 'none';
+        document.getElementById("ajuda1mobile").style.display = 'none';
+        document.getElementById("ajuda2mobile").style.display = 'block';
+        document.getElementById("ajuda3mobile").style.display = 'none';
+        document.getElementById("ajuda4mobile").style.display = 'none';
+        document.getElementById("ajuda5mobile").style.display = 'none';
+      }
 }
 function mudatexto3() {
-    document.getElementById("ajuda3").style.display = 'block';
-    document.getElementById("ajuda1").style.display = 'none';
-    document.getElementById("ajuda2").style.display = 'none';
-    document.getElementById("ajuda4").style.display = 'none';
-    document.getElementById("ajuda5").style.display = 'none';
+    if (window.matchMedia("(min-width:805px)").matches) {
+        document.getElementById("ajuda1").style.display = 'none';
+        document.getElementById("ajuda2").style.display = 'none';
+        document.getElementById("ajuda3").style.display = 'block';
+        document.getElementById("ajuda4").style.display = 'none';
+        document.getElementById("ajuda5").style.display = 'none';
+        document.getElementById("ajuda1mobile").style.display = 'none';
+        document.getElementById("ajuda2mobile").style.display = 'none';
+        document.getElementById("ajuda3mobile").style.display = 'none';
+        document.getElementById("ajuda4mobile").style.display = 'none';
+        document.getElementById("ajuda5mobile").style.display = 'none';
+      } else {
+        document.getElementById("ajuda1").style.display = 'none';
+        document.getElementById("ajuda2").style.display = 'none';
+        document.getElementById("ajuda3").style.display = 'none';
+        document.getElementById("ajuda4").style.display = 'none';
+        document.getElementById("ajuda5").style.display = 'none';
+        document.getElementById("ajuda1mobile").style.display = 'none';
+        document.getElementById("ajuda2mobile").style.display = 'none';
+        document.getElementById("ajuda3mobile").style.display = 'block';
+        document.getElementById("ajuda4mobile").style.display = 'none';
+        document.getElementById("ajuda5mobile").style.display = 'none';
+      }
 }
 function mudatexto4() {
-    document.getElementById("ajuda4").style.display = 'block';
-    document.getElementById("ajuda1").style.display = 'none';
-    document.getElementById("ajuda2").style.display = 'none';
-    document.getElementById("ajuda3").style.display = 'none';
-    document.getElementById("ajuda5").style.display = 'none';
+    if (window.matchMedia("(min-width:805px)").matches) {
+        document.getElementById("ajuda1").style.display = 'none';
+        document.getElementById("ajuda2").style.display = 'none';
+        document.getElementById("ajuda3").style.display = 'none';
+        document.getElementById("ajuda4").style.display = 'block';
+        document.getElementById("ajuda5").style.display = 'none';
+        document.getElementById("ajuda1mobile").style.display = 'none';
+        document.getElementById("ajuda2mobile").style.display = 'none';
+        document.getElementById("ajuda3mobile").style.display = 'none';
+        document.getElementById("ajuda4mobile").style.display = 'none';
+        document.getElementById("ajuda5mobile").style.display = 'none';
+      } else {
+        document.getElementById("ajuda1").style.display = 'none';
+        document.getElementById("ajuda2").style.display = 'none';
+        document.getElementById("ajuda3").style.display = 'none';
+        document.getElementById("ajuda4").style.display = 'none';
+        document.getElementById("ajuda5").style.display = 'none';
+        document.getElementById("ajuda1mobile").style.display = 'none';
+        document.getElementById("ajuda2mobile").style.display = 'none';
+        document.getElementById("ajuda3mobile").style.display = 'none';
+        document.getElementById("ajuda4mobile").style.display = 'block';
+        document.getElementById("ajuda5mobile").style.display = 'none';
+      }
+    
 }
 function mudatexto5() {
-    document.getElementById("ajuda5").style.display = 'block';
-    document.getElementById("ajuda1").style.display = 'none';
-    document.getElementById("ajuda2").style.display = 'none';
-    document.getElementById("ajuda3").style.display = 'none';
-    document.getElementById("ajuda4").style.display = 'none';
+    if (window.matchMedia("(min-width:805px)").matches) {
+        document.getElementById("ajuda1").style.display = 'none';
+        document.getElementById("ajuda2").style.display = 'none';
+        document.getElementById("ajuda3").style.display = 'none';
+        document.getElementById("ajuda4").style.display = 'none';
+        document.getElementById("ajuda5").style.display = 'block';
+        document.getElementById("ajuda1mobile").style.display = 'none';
+        document.getElementById("ajuda2mobile").style.display = 'none';
+        document.getElementById("ajuda3mobile").style.display = 'none';
+        document.getElementById("ajuda4mobile").style.display = 'none';
+        document.getElementById("ajuda5mobile").style.display = 'none';
+      } else {
+        document.getElementById("ajuda1").style.display = 'none';
+        document.getElementById("ajuda2").style.display = 'none';
+        document.getElementById("ajuda3").style.display = 'none';
+        document.getElementById("ajuda4").style.display = 'none';
+        document.getElementById("ajuda5").style.display = 'none';
+        document.getElementById("ajuda1mobile").style.display = 'none';
+        document.getElementById("ajuda2mobile").style.display = 'none';
+        document.getElementById("ajuda3mobile").style.display = 'none';
+        document.getElementById("ajuda4mobile").style.display = 'none';
+        document.getElementById("ajuda5mobile").style.display = 'block';
+      }
 }
