5caff15d0d168743ec428e0aeba3c14eb15892ec
João Victor de Lima <joaolima675@outlook.com>
2022-07-12 11:24:30 -0300

    Ajuste página cadastro/login e reestruturação funcionalidades.js

--- src/js/funcionalidades.js
+++ src/js/funcionalidades.js
@@ -1,18 +1,20 @@
+// Function Dark-Mode (All pages)
+
 const $html = document.querySelector('html')
 const dark = document.getElementById('btn-dark')
 
  window.onload = function(){
     $tema = localStorage.getItem('tema');
 
-    if($tema == "dark"){// SE JÁ ESTÁ DARK, QUER DIZER QUE ELE JÀ ESTÀ NO MODO ESCURO
+    // Get the value of the object local storage, if its "dark", the page opens in dark mode
+    if($tema == "dark"){
         $html.classList.toggle('dark-mode')
     }
 }
 
 function darkmode(){
     $tema = localStorage.getItem('tema');
-    
-    if($tema == "dark"){ // SE JÁ ESTÁ DARK, QUER DIZER QUE ELE JÀ ESTÀ NO MODO ESCURO
+    if($tema == "dark"){ // If its 'dark' in the localStorage variable, the page is in dark-mode. (The button switch to light-mode)
         $html.classList.toggle('dark-mode');
         localStorage.setItem('tema', 'light');
     }else{
@@ -22,11 +24,14 @@ function darkmode(){
     }
 }
 
+// Function Dropdown Menu (All pages)
+
 function dropdown(){
     const nav = document.getElementById ('menu_drop'); 
-    nav.classList.toggle ('active');
+    nav.classList.toggle('active');
 }
 
+// 
 function envia_contato(){
     alert("Enviou legal!");
 }
@@ -35,3 +40,144 @@ function acaoBotao(){
     alert("Obrigado por seu feedback!");
 }
 
+// Function Page Register - Verify Front-End
+
+//Variables Front-End Verification
+const input_name = document.getElementById('nome');
+const input_lastname = document.getElementById('sobrenome');
+const input_cpf = document.getElementById('cpf');
+const input_email = document.getElementById('email');
+const input_city = document.getElementById('cidade');
+
+//Variables Show/Hidden Password
+var btn_senha = document.getElementById('btn-show-hidden-passowrd')
+var input_password = document.getElementById('senha');
+
+// Verify Name
+input_name.addEventListener('blur', () =>{
+    input_name.checkValidity();
+    if(input_name.validity.valid){
+        input_name.classList.remove('invalid');
+    }else{
+        input_name.classList.add('invalid');
+    }
+});
+
+// Verify Last Name
+input_lastname.addEventListener('blur', () =>{
+    input_lastname.checkValidity();
+    if(input_lastname.validity.valid){
+        input_lastname.classList.remove('invalid');
+    }else{
+        input_lastname.classList.add('invalid');
+    }
+});
+
+// Verify CPF 
+input_cpf.addEventListener('blur', () =>{
+    cpf.classList.remove("invalid");
+
+    let valor = document.getElementById('cpf').value;
+    let cpfSeparado = [];
+    let cpfSeparado2 = [];
+    let soma = 0, resto = 0;
+
+    valor = valor.replace('.', '');
+    valor = valor.replace('.', '');  
+    valor = valor.replace('-', ''); 
+
+    if(valor == '11111111111' || 
+        valor == '22222222222' || 
+        valor == '33333333333' ||
+        valor == '44444444444' || 
+        valor == '55555555555' || 
+        valor == '66666666666' ||
+        valor == '77777777777' || 
+        valor == '88888888888' || 
+        valor == '99999999999' || 
+        valor == '00000000000' || 
+        valor.length < 11){
+
+        cpf.classList.add("invalid");
+
+    }else{
+        // Separando os Dígitos do CPF 
+        for(let x = 0, j = 1; x < 11; x++,j++){
+            if(j == 12){
+                cpfSeparado[x] = valor.substring((x-2),(x-1));
+            }else{
+                cpfSeparado[x] = valor.substring(x,j);
+            }
+        }
+        // Atribuição dos Números Verificadores
+        let dez = cpfSeparado[9];
+        let onze = cpfSeparado[10];
+
+        // PRIMEIRO DÌGITO VERIFICADOR 
+        // Convert - String to INT
+        for (let x = 0; x < 9; x++) {
+            cpfSeparado2[x] = cpfSeparado[x];
+        }
+        
+        for (let x = 0, y = 10; x < 9; x++, y--) {
+            cpfSeparado2[x] = cpfSeparado2[x] * y;
+            soma += cpfSeparado2[x];
+        } 
+        resto = (soma * 10) % 11;
+        
+        if(resto == 10) {resto = 0;}
+
+        if(resto == dez){
+            resto = 0;
+            soma = 0;
+            for (let x = 0; x < 10; x++) {
+                cpfSeparado2[x] = cpfSeparado[x];
+            }
+            for (let x = 0, y = 11; x < 10; x++, y--) {
+                cpfSeparado2[x] = cpfSeparado2[x] * y;
+                soma +=cpfSeparado2[x];
+            }
+            resto = (soma * 10) % 11;
+            if(resto == 10) {resto = 0;}
+            
+            if( resto == onze){
+                cpf.classList.remove("invalid")
+                cpf.style.border = "solid green";
+            }else{
+                cpf.classList.add("invalid")
+            }
+            
+        }else{
+            cpf.classList.add("invalid")
+        }
+    }
+});
+
+// Verify email
+input_email.addEventListener('blur', () =>{
+    input_email.checkValidity();
+    if(input_email.validity.valid){
+        input_email.classList.remove('invalid');
+    }else{
+        input_email.classList.add('invalid');
+    }
+});
+
+// Verify City
+input_city.addEventListener('blur', () =>{
+    input_city.checkValidity();
+    if(input_city.validity.valid){
+        input_city.classList.remove('invalid');
+    }else{
+        input_city.classList.add('invalid');
+    }
+});
+
+// Show-Hidden Password
+btn_senha.addEventListener('click', () => {
+    if(input_password.type == "password"){
+        input_password.type = "text";
+    }else{
+        input_password.type = "password";
+    }
+});
--- src/pages/cadastro.php
+++ src/pages/cadastro.php
@@ -164,22 +164,19 @@
 
             <article id="container_direita">
                 <!-- USUÁRIO SEM $_SESSION!-->
-                <?php
-                    if(isset($_SESSION['nome'])){
-                ?>
                     <form method = "POST" action="">
                     <h1>Cadastro</h1>
                      <div class="form_nome">
-                        <input type="text" placeholder="Nome" id="nome" name="nome" value="<?php echo($_SESSION['nome']);?>">
-                        <input type="text" placeholder="Sobrenome" id="sobrenome" name="sobre_nome" value="<?php echo($_SESSION['sobre_nome']);?>">
+                        <input type="text" class = ''placeholder="Nome" id="nome" name="nome"  pattern="[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]+$" value="<?php echo (isset($_SESSION['nome'])) ? $_SESSION['nome'] : "";?>">
+                        <input type="text" class = '' placeholder="Sobrenome" id="sobrenome" name="sobre_nome" pattern="[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]+$" value="<?php echo (isset($_SESSION['sobre_nome'])) ? $_SESSION['sobre_nome'] : "";?>">
                      </div>   
                     
                     <div>
-                        <input type="text" placeholder="CPF" name="cpf" id="cpf" onChange="validaCpf()" maxlength="11" onkeypress="$(this).mask('000.000.000-00')" value="<?php echo($_SESSION['cpf']);?>">
-                        <input type="email" placeholder="E-mail" name="email" id="email" value=<?php echo($_SESSION['email']);?>>
-                        <input type="password" placeholder="Senha" name="senha" id="senha">
-                        <input type="text" placeholder="Telefone" maxlength="11" name="telefone" id="telefone" onkeypress="$(this).mask('(00) 00000-0000')" value="<?php echo($_SESSION['telefone']);?>">
-                        <input type="text" placeholder="Cidade" name="cidade" id="cidade" value="<?php echo($_SESSION['cidade']);?>">
+                        <input type="text" class = '' placeholder="CPF" name="cpf" id="cpf" maxlength="11" onkeypress="$(this).mask('000.000.000-00')" value="<?php echo (isset($_SESSION['cpf'])) ? $_SESSION['cpf'] : "";?>">
+                        <input type="email" class = '' placeholder="E-mail" name="email" id="email" value="<?php echo (isset($_SESSION['email'])) ? $_SESSION['email'] : "";?>">
+                        <div id = "div-senha"><input type="password" class = '' placeholder="Senha" name="senha" id="senha"> <span id = "btn-show-hidden-passowrd"></span></div>
+                        <input type="text" class = '' placeholder="Telefone" maxlength="11" name="telefone" id="telefone" onkeypress="$(this).mask('(00) 00000-0000')" value="<?php echo (isset($_SESSION['telefone'])) ? $_SESSION['telefone'] : "";?>">
+                        <input type="text" class = '' placeholder="Cidade" name="cidade" id="cidade" pattern="[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]+$" value="<?php echo (isset($_SESSION['cidade'])) ? $_SESSION['cidade'] : "";?>">
                     </div>
                     
                     <div class="radio">
@@ -194,40 +191,7 @@
                     </div>
                     <input type="submit" id="btn_cadastrar">
                 </form>
-                <?php } 
-
-                else{ ?>  
-                        <form method = "POST" action="">
-                        <h1>Cadastro</h1>
-                         <div class="form_nome">
-                            <input type="text" placeholder="Nome" id="nome" name="nome" value="">
-                            <input type="text" placeholder="Sobrenome" id="sobrenome" name="sobre_nome" value="">
-                         </div>   
                         
-                        <div>
-                            <input type="text" placeholder="CPF" name="cpf" id="cpf" onChange="validaCpf()" maxlength="11" onkeypress="$(this).mask('000.000.000-00')" value="">
-                            <input type="email" placeholder="E-mail" name="email" id="email" value="">
-                            <input type="password" placeholder="Senha" name="senha" id="senha">
-                            <input type="text" placeholder="Telefone" maxlength="11" name="telefone" id="telefone" onkeypress="$(this).mask('(00) 00000-0000')" value="">
-                            <input type="text" placeholder="Cidade" name="cidade" id="cidade" value="">
-                        </div>
-                        
-                        <div class="radio">
-                                <span><input type="radio" name="tipo" id="pessoaFisica" class="radio" value="1"><label for="pessoaFisica">Pessoa Fisica</label></span>
-                                <span><input type="radio" name="tipo" id="pessoaJuridica" class="radio" value="2"><label for="pessoaJuridica">Pessoa Juridica</label></span>
-                        </div>
-                        <!--<button id="btn_cadastrar" onclick="fazCadastro()">Cadastrar</button>-->
-                        
-                        <div class="redes_sociais">
-                            <button id="btn_facebook">Facebook</button>
-                            <button id="btn_google">Google</button>
-                        </div>
-                        <input type="submit" id="btn_cadastrar">
-                    </form>
-                <?php } 
-                ?>     
-                 
-
                 <div id = fora_form>
                     <!-- <button id="btn_cadastrar">Cadastrar</button> -->
                     <a href="login.php" class="possui_cadastro">Já possuo cadastro</a>
@@ -237,7 +201,6 @@
     </article>    
 </body>
 <script src="../js/funcionalidades.js"></script>
-<script src="../js/cpf.js"></script>
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.15/jquery.mask.min.js"></script>
 </html>
